82117bed167178d9082199aa86fc1484f7a1b652
Nichlas <nichlas.flindt.krabbe@gmail.com>
2025-04-15 11:38:46 +0200

    Fjernet kontaktformular knappen herfra

--- src/views/lejlighedsudlejningview.vue
+++ src/views/lejlighedsudlejningview.vue
@@ -1,40 +1,19 @@
 <template>
   <div class="container">
-    <h1>Lejlighed til Udlejning</h1>
     <lejlighedInfo />
     <slider />
     <googleMaps />
-    <kontaktformularComponent
-      :is-open="isContactFormOpen"
-      @close="closeContactForm"
-    />
-    <button @click="openContactForm" class="contact-btn">
-      Kontakt udlejer
-    </button>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue"
 import lejlighedInfo from "../components/lejlighedInfo.vue"
 import slider from "../components/slider.vue"
-import kontaktformularComponent from "../components/kontaktformularComponent.vue"
+
 import googleMaps from "../components/googleMaps.vue"
-
-// Reaktiv tilstand til at åbne/lukke kontaktformularen
-const isContactFormOpen = ref(false)
-
-const openContactForm = () => {
-  isContactFormOpen.value = true
-}
-
-const closeContactForm = () => {
-  isContactFormOpen.value = false
-}
 </script>
 
 <style scoped>
-/* Kontakt-knap */
 .contact-btn {
   position: fixed;
   bottom: 20px;
@@ -48,5 +27,9 @@ const closeContactForm = () => {
   font-size: 16px;
   z-index: 3;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.2);
+}
+
+.contact-btn:hover {
+  background-color: #225e5f;
 }
 </style>
